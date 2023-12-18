f45534fe45dee55a02318281778235f9728c749b
kalaichandran <kalaichandran369@gmail.com>
2023-12-18 10:46:01 +0400

    ui: view works btn added scroll

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,12 +18,12 @@ const Home = () => {
            Currently, I’m focused on building responsive full-stack web applications.
         </p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+          <Link to='work' className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600' smooth={true} duration={500}>
             View Work
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3 ' />
             </span>
-          </button>
+          </Link>
         </div>
       </div>
     </div>
